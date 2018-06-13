8f823ea47990958bcd01ce216afd064014965ddc
Federico Lo Grasso <lograssofede@gmail.com>
2018-06-13 14:12:44 -0300

    a

--- includes/phpFunctions.php
+++ includes/phpFunctions.php
@@ -230,7 +230,7 @@ function procesarMensaje($idmensaje)
         }
         else
         {
-            setValueDb("messages","message_id,date_received,date_read,from_user_id,from_name,text,order_id,moderada,to_user_id,to_user_name","'$idmensaje','". $result["body"] ->date_received ."','".$result["body"] ->date_read   ."','". $from->user_id . "','" . $from->name ."','" .  quitarSaltos($text->plain) . "','". $result["body"] ->resource_id ."','" . $moderation->status ."','". $to->to_user_id . "','" . $to->to_user_name . "','" ."'");
+            setValueDb("messages","message_id,date_received,date_read,from_user_id,from_name,text,order_id,moderada,to_user_id,to_user_name","'$idmensaje','". $result["body"] ->date_received ."','".$result["body"] ->date_read   ."','". $from->user_id . "','" . $from->name ."','" .  quitarSaltos($text->plain) . "','". $result["body"] ->resource_id ."','" . $moderation->status ."','". $to->to_user_id . "','" . $to->to_user_name ."'");
         }
     }
     else
