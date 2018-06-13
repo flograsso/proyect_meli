52809df0c3de8e27017c915c2e7dc0bbb706ac4f
Federico Lo Grasso <lograssofede@gmail.com>
2018-06-13 11:07:09 -0300

    a

--- js/functions.js
+++ js/functions.js
@@ -36,7 +36,8 @@ $(document).ready(function(){
                data.push(myObj[x].fechaRespuesta);
                addRow("table-listaPreguntas",data); 
             }
-            $('#table-listaPreguntas').DataTable();     
+            $('#table-listaPreguntas').DataTable();   
+            debugger;  
             var table = $('#table-listaPreguntas').DataTable({
                 "columnDefs": [{
                     "visible": false,
