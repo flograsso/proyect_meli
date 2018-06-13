62d3ec4106f9cba416deeea7e2a9dfddfefaec0e
Federico Lo Grasso <lograssofede@gmail.com>
2018-06-13 11:29:41 -0300

    a

--- js/lib/datatables/datatables-init.js
+++ js/lib/datatables/datatables-init.js
@@ -2,28 +2,6 @@ $(document).ready(function() {
     
         $('#table-listaPreguntas').DataTable();
         
-        $(document).ready(function() {
-            var table = $('#table-listaPreguntas').DataTable({
-                "columnDefs": [{
-                    "visible": false,
-                    "targets": 2
-                }],
-                "order": [
-                    [2, 'asc']
-                ],
-                "displayLength": 25,
-   
-            });
-            // Order by the grouping
-            $('#table-listaPreguntas tbody').on('click', 'tr.group', function() {
-                var currentOrder = table.order()[0];
-                if (currentOrder[0] === 2 && currentOrder[1] === 'asc') {
-                    table.order([2, 'desc']).draw();
-                } else {
-                    table.order([2, 'asc']).draw();
-                }
-            });
-        });
     });
     $('#example23').DataTable({
         dom: 'Bfrtip',
