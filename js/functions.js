29449491360a03f8768e0afcd0c9dbf73548d642
Federico Lo Grasso <lograssofede@gmail.com>
2018-06-13 10:35:17 -0300

    a

--- js/functions.js
+++ js/functions.js
@@ -36,7 +36,7 @@ $(document).ready(function(){
                data.push(myObj[x].fechaRespuesta);
                addRow("table-listaPreguntas",data); 
             }     
-              
+            initTable();    
         }
     });
 
@@ -50,4 +50,45 @@ function addRow(table,content)
         $("#"+table + " tbody").append("<td>"+content[x]+"</td>"); 
     }
     $("#"+table + " tbody").append("</tr>");
+}
+
+function initTable()
+{
+    $('#table-listaPreguntas').DataTable();
+    $(document).ready(function() {
+        var table = $('#table-listaPreguntas').DataTable({
+            "columnDefs": [{
+                "visible": false,
+                "targets": 2
+            }],
+            "order": [
+                [2, 'asc']
+            ],
+            "displayLength": 25,
+            "drawCallback": function(settings) {
+                var api = this.api();
+                var rows = api.rows({
+                    page: 'current'
+                }).nodes();
+                var last = null;
+                api.column(2, {
+                    page: 'current'
+                }).data().each(function(group, i) {
+                    if (last !== group) {
+                        $(rows).eq(i).before('<tr class="group"><td colspan="5">' + group + '</td></tr>');
+                        last = group;
+                    }
+                });
+            }
+        });
+        // Order by the grouping
+        $('#table-listaPreguntas tbody').on('click', 'tr.group', function() {
+            var currentOrder = table.order()[0];
+            if (currentOrder[0] === 2 && currentOrder[1] === 'asc') {
+                table.order([2, 'desc']).draw();
+            } else {
+                table.order([2, 'asc']).draw();
+            }
+        });
+    });
 }
