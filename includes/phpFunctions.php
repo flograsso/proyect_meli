d00899521627425c253241bfe594b26dc6954d6f
Federico Lo Grasso <lograssofede@gmail.com>
2018-06-15 15:01:07 -0300

    a

--- includes/phpFunctions.php
+++ includes/phpFunctions.php
@@ -248,14 +248,13 @@ function diffDatesSeg($dateA,$dateQ)
 
 function convertirFecha($date)
 {
-    $fecha= date_create($date, timezone_open('America/Argentina/Buenos_Aires'));
+    $fecha= date_create($date, timezone_open('UTC'));
 
 
     echo date_format($fecha, 'Y-m-d H:i:sP') . "<br>";
-    date_timezone_set($fecha, timezone_open('UTC'));
+    date_timezone_set($fecha, timezone_open('America/Argentina/Buenos_Aires'));
     echo date_format($fecha, 'Y-m-d H:i:sP') . "<br>";
-    date_timezone_set($fecha, timezone_open('Europe/London'));
-    echo date_format($fecha, 'Y-m-d H:i:sP') . "<br>";
+
 
 
     return date_format($fecha, 'Y-m-d H:i:sP');
