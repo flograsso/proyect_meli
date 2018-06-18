4014b4b11e834908e4c87871e1d510cc8463454c
Federico Lo Grasso <lograssofede@gmail.com>
2018-06-18 11:21:08 -0300

    a

--- includes/connection.php
+++ includes/connection.php
@@ -1,11 +1,16 @@
 <?php
-
+/*	
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 //database
 $DB_HOST=$url["host"];
 $DB_USERNAME=$url["user"];
 $DB_PASSWORD=$url["pass"];
 $DB_NAME=substr($url["path"], 1);
+*/
+$DB_HOST=getenv("db_host");
+$DB_USERNAME=getenv("branded_azd5889f");
+$DB_PASSWORD=getenv("n-2(NBApr5~e");
+$DB_NAME=getenv("branded_melidb");
 
 //get connection
 //(MySQLi Object-Oriented)
